0b2f570510c3cc0ebb64ef159f066c4a04473c40
Natja-baobao <Natja_Python_Java@outlook.com>
2016-06-29 22:05:13 +0800

    configuration

---
+++ Map/src/my/map/xfy/MapActivity.java
@@ -0,0 +1,20 @@
+package my.map.xfy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.BDNotifyListener;//假如用到位置提醒功能，需要import该类
+import com.baidu.location.Poi;
+
+public class MapActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
