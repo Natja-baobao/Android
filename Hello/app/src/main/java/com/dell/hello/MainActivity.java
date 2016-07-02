874e23e5f6cc9b6bdfdbc5181793376685eeee88
Natja-baobao <Natja_Python_Java@outlook.com>
2016-07-02 19:47:39 +0800

    baidumap

---
+++ Hello/app/src/main/java/com/dell/hello/MainActivity.java
@@ -0,0 +1,70 @@
+package com.dell.hello;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.LogoPosition;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView = null;
+    BaiduMap mBaiduMap = null;
+    //public final static String EXTRA_MESSAGE = "com.dell.hello.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        //设置百度logo位置
+        mMapView.setLogoPosition(LogoPosition.logoPostionleftTop);
+        //开启交通图
+        mBaiduMap.setTrafficEnabled(true);
+        //设置卫星地图
+        //mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+        //开启热力图
+        //mBaiduMap.setBaiduHeatMapEnabled(true);
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+}
+    /*
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    */
+
